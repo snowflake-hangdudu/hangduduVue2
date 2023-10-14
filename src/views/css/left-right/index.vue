e49b6d6d9514f80a8134d785f0e47480704d7a23
snowflake-hangdudu <15551359775@163.com>
2023-10-14 10:25:31 +0800

    样式

--- src/views/css/left-right/index.vue
+++ src/views/css/left-right/index.vue
@@ -1,15 +1,94 @@
 <template>
-    <div >
-      
+    <div class="container">
+      <div class="container1">
+        <div class="left-half">姓名
+        </div>
+        <div class="right-half">
+          <el-input>
+
+          </el-input>
+        </div>
+      </div>
+      <div class="line"></div>
+      <div class="container2">
+        <div class="left-half">姓名
+        </div>
+        <div class="right-half">
+          <el-input>
+
+          </el-input>
+        </div>
+      </div>
+
     </div>
+
   </template>
-  
+
   <script>
   export default {
-  
+    data() {
+      return {
+
+      }
+    }
+
   }
   </script>
-  
-  <style>
-  
+
+  <style scoped lang="scss">
+    .container{
+      display: flex;
+
+
+      .container1 {
+      display: flex;
+      width: 50%;
+      background-color: aqua;
+      padding: 10px;
+
+      .left-half {
+        width: 20%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        background-color: pink;
+        padding-right:10px;
+        flex-wrap: nowrap;
+      }
+
+      .right-half {
+        background-color: yellow;
+        flex-grow: 1;
+      }
+    }
+    .line{
+      border: 1px solid red;
+    }
+
+
+    .container2 {
+      display: flex;
+      width: 50%;
+      background-color: rgb(127, 54, 243);
+      padding: 10px;
+
+      .left-half {
+        width: 20%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        background-color: pink;
+        padding-right:10px;
+        flex-wrap: nowrap;
+      }
+
+      .right-half {
+        background-color: yellow;
+        flex-grow: 1;
+      }
+    }
+
+    }
+
+
   </style>
--- src/views/debug/index.vue
+++ src/views/debug/index.vue
@@ -1,40 +1,24 @@
 <template>
   <div class="app-container">
     <div class="header">
-      <el-button type="primary" @click="exportExcel()">导出</el-button>
+
     </div>
   </div>
 </template>
 
 <script>
-import { dataConversionUtil } from "@/utils/exportExcel";
+
 export default {
   data() {
     return {
-      // data: [
-      //   { name: "阿梅", sex: "女", age: "16" },
-      //   { name: "阿三", sex: "男", age: "24" },
-      // ],
-      // tabelHeader: [["id", "姓名", "性别", "年龄"]],
+
     };
   },
   mounted() {
-    console.log(dataConversionUtil, "dataConversionUtil");
+
   },
   methods: {
-    exportExcel() {
-      var data = [
-        { name: "阿三", sex: "男", age: "24" },
-        { name: "阿梅", sex: "女", age: "16" },
-      ];
-      var tableHeader = [["序号", "姓名", "性别", "年龄"]];
-      var dataList = [];
-      data.forEach((item, index) => {
-        dataList.push([index + 1, item.name, item.sex, item.age]);
-      });
-      dataConversionUtil.dataToExcel("人员", tableHeader, dataList);
-      this.$message.success("导出成功!");
-    },
+
   },
 };
 </script>
