cf4e94aa81223a76ecf4f2044ae95c4ec355a831
snowflake-hangdudu <15551359775@163.com>
2023-11-02 18:21:25 +0800

    111

--- src/views/debug/index.vue
+++ src/views/debug/index.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="app-container">
     <div class="header">
-
+      <input
+        type="text"
+        v-model="name"
+        @keydown="copy($event)"
+        ref="hangdudu"
+      />
+      <Love :name="name" :age="age + 1"></Love>
     </div>
   </div>
 </template>
 
 <script>
-
+import Love from "./love";
 export default {
   data() {
     return {
-
+      name: "hangdudu",
+      age: 20,
     };
   },
-  mounted() {
-
+  components: {
+    Love,
   },
+  mounted() {},
   methods: {
-
+    copy(e) {
+      let data = new Date();
+      console.log(data.toLocaleString());
+      console.log(this.$refs);
+      console.log(this.$refs.hangdudu);
+    },
   },
 };
 </script>
---
+++ src/views/debug/love/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ name }}
+    {{ age }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    name: {
+      type: String, //类型
+      required: true, //必要性
+      default: "SS", //默认值
+    },
+    age: {
+      type: Number,
+      require: true,
+      default: 20,
+    },
+  },
+};
+</script>
+
+<style></style>
